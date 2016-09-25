dd93ac14dd2d8d44566cf31a16808fb208e99ccb
Gabriel Estes <gabriel.estes@gmail.com>
2016-09-24 20:22:47 -0400

    Create method to output by frequency of reviews.

--- popular_movies.rb
+++ popular_movies.rb
@@ -8,48 +8,31 @@ class PopularMovies < Movie
   def initialize
   end
 
-  def delete_last_element
+  def delete_first_last
     @user_rating_data.each do |user|
+      user.delete_at(3)
+      user.delete_at(2)
       user.delete_at(0)
-      user.delete_at(1)
-      user.delete_at(1)
     end
-    @user_rating_data
   end
 
   def create_movie_hash
-    counts = Hash.new(0)
-    @user_rating_data.each { |id| counts[id] += 1 }
-    puts counts
+    @frequency = Hash.new(0)
+    @user_rating_data.sort_by { |id| @frequency[id] += 1 }
   end
 
-  def single_out_by_count
-    @movie_review_storage = []
-    @user_rating_data.each do |user|
-      @movie_review_storage << user[1]
-    end
-    @movie_review_storage
+  def sort_by_frequency
+    @most_frequent_movies = @frequency.sort_by { |_k, v| -v }[0..200]
+    p @most_frequent_movies
   end
-
-  # def store_movie_ratings
-  #   @all_ratings = []
-  #   @user_rating_data.each do |user|
-  #     if return_average_rating >= 3
-  #       @ratings << user[2].to_i
-  #     end
-  #   end
-  #   puts "The ratings for this title are #{@ratings.flatten}."
-  # end
-  #
-  # def remove_low_ratings
-  # end
-end
 
 def main
   top_list = PopularMovies.new
   top_list.create_user_arrays
-  top_list.delete_last_element
+  top_list.delete_first_last
   top_list.create_movie_hash
+  top_list.sort_by_frequency
+  top_list.make_movie_lines_arrays
 end
 
 main if __FILE__ == $PROGRAM_NAME
